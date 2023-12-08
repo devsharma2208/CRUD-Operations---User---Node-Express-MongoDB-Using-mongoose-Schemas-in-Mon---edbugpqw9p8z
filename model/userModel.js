ab0b94eccfb0ee31418c7701e69966c28c1482ce
Dev Sharma <121969298+devsharma2208@users.noreply.github.com>
2023-12-09 00:58:25 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
   // Implement the user schema fields:
   // name as String and
   // email as a String,
+  name : String,
+  email : String,
 });
-
+const User = mongoose.model('user', userSchema);
+module.export = User;
 // Create and export the User model
